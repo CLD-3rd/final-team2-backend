8273fbab4b894dbe8dbe606b71d31a85088aa419
cjsqudwns <cjsqudwns2781@gmail.com>
2025-07-21 13:27:25 +0900

    init: spring boot

---
+++ src/main/java/com/goteego/GotEEgoApplication.java
@@ -0,0 +1,13 @@
+package com.goteego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GotEEgoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GotEEgoApplication.class, args);
+    }
+
+}
