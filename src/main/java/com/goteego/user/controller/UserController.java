5731cf9371917e57d3b31a665b95fd331df3a9ad
junbro <gjwnsgud4016@gmail.com>
2025-07-24 20:50:28 +0900

    feat: 채팅 관련 대시보드 html 추가 및 UserController 추가

--- src/main/java/com/goteego/user/controller/UserController.java
+++ src/main/java/com/goteego/user/controller/UserController.java
@@ -6,6 +6,7 @@ import com.goteego.user.dto.UserDto;
 import com.goteego.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,10 @@ public class UserController {
 
     private final UserService userService;
 
+    @GetMapping("/me")
+    public ResponseEntity<UserDto> getCurrentUser(@AuthenticationPrincipal User user) {
+        return ResponseEntity.ok(new UserDto(user.getId(), user.getOauthInfo().getOauthEmail()));
+    }
     @GetMapping
     public List<UserDto> searchAllUsers(@AuthenticationPrincipal User user) {
         return userService.findAll();
