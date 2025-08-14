4b9ca9b981510502a321fd3c90eeb2cbed1c1328
junbro <gjwnsgud4016@gmail.com>
2025-08-14 16:33:39 +0900

    fix: Redis 스캔 비활성화

--- src/main/java/com/goteego/GotEEgoApplication.java
+++ src/main/java/com/goteego/GotEEgoApplication.java
@@ -3,9 +3,10 @@ package com.goteego;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication//(exclude = MongoDataAutoConfiguration.class)
+@SpringBootApplication(exclude = RedisRepositoriesAutoConfiguration.class )
 @EnableJpaAuditing
 public class GotEEgoApplication {
 
--- src/main/java/com/goteego/global/redis/RedisConfig.java
+++ src/main/java/com/goteego/global/redis/RedisConfig.java
@@ -16,11 +16,13 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @EnableCaching // 캐시 활성화
+@EnableRedisRepositories(basePackages = {})
 public class RedisConfig {
 
     @Value("${spring.data.redis.password}")
