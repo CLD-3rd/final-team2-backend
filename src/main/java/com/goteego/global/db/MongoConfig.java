915c9077b52cd443acf3d96f2196b07e7978f93f
junbro <gjwnsgud4016@gmail.com>
2025-08-14 15:43:23 +0900

    chore: yml 주석 처리 해제

--- src/main/java/com/goteego/global/db/MongoConfig.java
+++ src/main/java/com/goteego/global/db/MongoConfig.java
@@ -1,15 +1,28 @@
 package com.goteego.global.db;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+
 @Configuration
-@EnableMongoRepositories(
-        basePackages = "com.goteego.chat.repository",
-        includeFilters = @Filter(type = FilterType.ASSIGNABLE_TYPE, classes = MongoRepository.class)
-)
+@EnableMongoRepositories(basePackages = "com.goteego.chat.repository",
+                        includeFilters = @Filter(type = FilterType.ASSIGNABLE_TYPE, classes = MongoRepository.class))
 public class MongoConfig {
+
+    @Bean
+    public MongoTemplate mongoTemplate(MongoClient mongoClient) {
+        return new MongoTemplate(mongoClient, "team2-mongodb");
+    }
 }
